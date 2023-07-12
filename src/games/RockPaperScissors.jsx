d95895c2a80855370af695bc7b9beb2b4d8ebd54
Denis <denis.leduc@protonmail.com>
2023-07-12 16:29:38 +0200

    Game states functions done

--- src/games/RockPaperScissors.jsx
+++ src/games/RockPaperScissors.jsx
@@ -15,12 +15,18 @@ function GameScreen(props) {
   return(
     <div>
       <h1>Game with score goal: {props.scoreGoal}</h1>
+      <button onClick={props.endGame}>End game</button>
     </div>
   )
 }
 
 function GameEnded(props) {
-
+  return(
+    <div>
+      <h1>Game finished</h1>
+      <button onClick={props.resetGame}>Reset</button>
+    </div>
+  )
 }
 
 function RockPaperScissors(props) {
@@ -34,12 +40,19 @@ function RockPaperScissors(props) {
   const startGame = () => {
     setGameState(1);
   }
+  const endGame = () => {
+    setGameState(2);
+  }
+  const resetGame = () => {
+    setGameState(0);
+  }
 
   return (
     <div id="rock-paper-scissors">
       <h1>Rock Paper Scissors</h1>
       {gameState === 0 ? <PreStartGame startGame={startGame} setTargetScore={setTargetScore} /> : <></>}
-      {gameState === 1 ? <GameScreen scoreGoal={wantedScore} /> :<></> }
+      {gameState === 1 ? <GameScreen scoreGoal={wantedScore} endGame={endGame} /> : <></> }
+      {gameState === 2 ? <GameEnded resetGame={resetGame} /> : <></> }
     </div>
   );
 }
