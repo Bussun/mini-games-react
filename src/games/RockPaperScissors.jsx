59bae8961e9a1863fc376c122fe4f6422da7de49
Denis <denis.leduc@protonmail.com>
2023-07-12 16:42:50 +0200

    targetScore isn't undefined anymore
    when player launches game without touching the input

--- src/games/RockPaperScissors.jsx
+++ src/games/RockPaperScissors.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useTranslation } from "react-i18next";
 
 function PreStartGame(props) {
   const {startGame, setTargetScore} = props;
@@ -6,32 +7,38 @@ function PreStartGame(props) {
     <div>
       <h1>Pre start screen</h1>
       <button onClick={startGame}>Start Game</button>
-      <input type="number" name="test" id="test" onChange={(e) => {setTargetScore(e.target.value)}}/>
+      {(() => {setTargetScore(5)})()}
+      <input type="number" name="test" id="test" value={5} onChange={(e) => {setTargetScore(e.target.value)}}/>
     </div>
   )
 }
 
 function GameScreen(props) {
+  const {scoreGoal, endGame} = props;
   return(
     <div>
-      <h1>Game with score goal: {props.scoreGoal}</h1>
-      <button onClick={props.endGame}>End game</button>
+      <h1>Game with score goal: {scoreGoal}</h1>
+      <button onClick={endGame}>End game</button>
     </div>
   )
 }
 
 function GameEnded(props) {
+  const {resetGame, finalWinner} = props;
+  const { t } = useTranslation();
   return(
     <div>
       <h1>Game finished</h1>
-      <button onClick={props.resetGame}>Reset</button>
+      <h2>The final winner is {finalWinner === 1 ? t("rps_winner_player") : finalWinner === 2 ? t("rps_winner_computer") : "undefined"}</h2>
+      <button onClick={resetGame}>Reset</button>
     </div>
   )
 }
 
 function RockPaperScissors(props) {
   const [gameState, setGameState] = useState(0); // 0 Not started; 1 Started; 2 Finished
-  const [wantedScore, setWantedScore] = useState(0);
+  const [wantedScore, setWantedScore] = useState(undefined);
+  const [finalWinner, setFinalWinner] = useState(undefined); // 1 for player one, 2 for computer/player 2
 
   const setTargetScore = (score) => {
     setWantedScore(Number.parseInt(score));
@@ -40,11 +47,14 @@ function RockPaperScissors(props) {
   const startGame = () => {
     setGameState(1);
   }
-  const endGame = () => {
+  const endGame = (winner) => {
+    setFinalWinner(winner);
     setGameState(2);
   }
   const resetGame = () => {
     setGameState(0);
+    setWantedScore(undefined);
+    setFinalWinner(undefined);
   }
 
   return (
@@ -52,7 +62,7 @@ function RockPaperScissors(props) {
       <h1>Rock Paper Scissors</h1>
       {gameState === 0 ? <PreStartGame startGame={startGame} setTargetScore={setTargetScore} /> : <></>}
       {gameState === 1 ? <GameScreen scoreGoal={wantedScore} endGame={endGame} /> : <></> }
-      {gameState === 2 ? <GameEnded resetGame={resetGame} /> : <></> }
+      {gameState === 2 ? <GameEnded resetGame={resetGame} finalWinner={finalWinner} /> : <></> }
     </div>
   );
 }
