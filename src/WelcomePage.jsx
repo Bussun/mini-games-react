dbc01908ead0cd91dc8b6489337dbc45ec146b78
Denis <denis.leduc@protonmail.com>
2023-07-15 02:01:00 +0200

    working on main page

--- src/WelcomePage.jsx
+++ src/WelcomePage.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { TbExternalLink } from 'react-icons/tb';
+import './WelcomePage.css';
 
 function WelcomePage(props) {
     const {t} = useTranslation();
@@ -8,12 +9,15 @@ function WelcomePage(props) {
     return(
         <div id='welcome-page'>
             <nav id="nav">
-                <a href="https://github.com/Bussun/mini-games-react" target="_blank">GitHub <TbExternalLink /></a>
+                <a className="center nav_link" href="https://github.com/Bussun/mini-games-react" target="_blank">GitHub <TbExternalLink /></a>
             </nav>
-            <h1>{t("mainPageTitle")}</h1>
+            <div className="content">
+            <h1 className="text_center mainPageTitle">{t("mainPageTitle")}</h1>
             <Link to="./games/rock-paper-scissors">Rock Paper Scissors</Link>
             <Link to="./games/tic-tac-toe">Tic Tac Toe</Link>
             <Link to="./games/etch-a-sketch">Etch-a-sketch</Link>
+            </div>
+            <footer>Copyright 2023 Denis LEDUC</footer>
         </div>
     )
 }
