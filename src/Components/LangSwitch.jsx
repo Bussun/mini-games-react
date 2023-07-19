8f52862a446f4142eb5012ea0ab33ff8a34b1d86
Denis <denis.leduc@protonmail.com>
2023-07-19 18:08:18 +0200

    tic tac toe logic

--- src/Components/LangSwitch.jsx
+++ src/Components/LangSwitch.jsx
@@ -9,6 +9,7 @@ function LangSwitch() {
       <select
         value={i18n.language}
         onChange={(e) => i18n.changeLanguage(e.target.value)}
+        style={{backgroundColor: 'rgb(24,24,27)', color: 'white', border: '1px solid gray', borderRadius: '5px', padding: '.5rem 1rem'}}
       >
         <option value="en">English</option>
         <option value="fr">Français</option>
--- src/games/TicTacToe.jsx
+++ src/games/TicTacToe.jsx
@@ -13,10 +13,85 @@ function PreStart(props) {
     )
 }
 
+function Cell(props) {
+    const {index, handleClick, board} = props;
+    return(
+        <button className={`ttt_gameBoard_square ttt_gameBoard_${index}`} onClick={() => {handleClick(index)}}>{board[index]}</button>
+    )
+}
+
+function Board(props) {
+    const {handleClick, board} = props;
+    return(
+        <div className='ttt_gameBoard'>
+            <Cell index={0} handleClick={handleClick} board={board}/>
+            <Cell index={1} handleClick={handleClick} board={board}/>
+            <Cell index={2} handleClick={handleClick} board={board}/>
+            <Cell index={3} handleClick={handleClick} board={board}/>
+            <Cell index={4} handleClick={handleClick} board={board}/>
+            <Cell index={5} handleClick={handleClick} board={board}/>
+            <Cell index={6} handleClick={handleClick} board={board}/>
+            <Cell index={7} handleClick={handleClick} board={board}/>
+            <Cell index={8} handleClick={handleClick} board={board}/>
+        </div>
+    )
+}
+
 function GameScreen(props) {
+    const {t} = useTranslation();
+
+    const {isP2Ai} = props;
+    const [currentPlayer, setCurrentPlayer] = useState(1);
+    const [board, setBoard] = useState([null,null,null,null,null,null,null,null,null]);
+
+    function handleClick(index) {
+        if (board[index] !== null) {
+            return;
+        }
+        let updatedBoard = board.slice();
+        let marker = currentPlayer === 1 ? 'X' : 'O';
+
+        updatedBoard[index] = marker;
+        let winner = checkWinner(updatedBoard);
+        if (winner !== null) {
+            // End game
+        }
+
+        console.log(isP2Ai);
+        if (isP2Ai === true) {
+            let computerMove = playRandomLegalMove(updatedBoard);
+            console.log(computerMove)
+            updatedBoard[computerMove] = 'O';
+            winner = checkWinner(updatedBoard);
+            if (winner !== null) {
+                // End game
+            }
+            console.log(updatedBoard)
+        }
+        else {
+            setCurrentPlayer(currentPlayer === 1 ? 2 : 1);
+        }
+
+        setBoard(updatedBoard);
+    }
+
+    function playRandomLegalMove(newBoard) {
+        let nullValues = newBoard.map((val, index) => {
+            if (val === null) {
+                return index;
+            }
+        });
+        let finalArray = nullValues.filter(val => val !== undefined);
+        let randomIndex = Math.floor(Math.random() * finalArray.length);
+        return finalArray[randomIndex];
+    }
+
+    function checkWinner(newBoard) {}
+
     return(
         <div className="ttt_gameScreen">
-            <h2>Game screen</h2>
+            <h2>texte au pif pour l'instant</h2>
+            <Board handleClick={handleClick} board={board} />
         </div>
     )
 }
@@ -32,13 +107,13 @@ function TicTacToe(props) {
     }, []);
 
     const [gameStarted, setGameState] = useState(false);
-    const [isP2Ai, setP2] = useState(false);
+    const [isP2Ai, setP2] = useState(true);
     
     return(
         <>
             <BackHome />
             <h1 className="ttt_mainTitle">{t("ttt_mainTitle")}</h1>
-            {gameStarted === false ? <PreStart setGameState={setGameState} /> : <GameScreen />}
+            {gameStarted === false ? <PreStart setGameState={setGameState} /> : <GameScreen isP2Ai={isP2Ai} setGameState={setGameState} />}
         </>
     )
 }
